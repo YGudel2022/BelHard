b75be88b58a134840f3f803cf4b49b0e20143157
YGudel2022 <info@beliso.by>
2022-12-04 17:00:37 +0300

    merge Yuri end Gudel

--- 4_task_1.py
+++ 4_task_1.py
@@ -2,10 +2,15 @@
 # 1. Заполнить список степенями числа 2 (от 2^1 до 2^n).
 
 nambers = int(input("Список от 2^1 до 2^n, где n =  "))
-nambers_tuple = {2**i for i in range(1, nambers + 1)}         # кортеж из от 2^1 до 2^n
-list_nambers_tuple = list(nambers_tuple)                      # преобразовали в список
-list_nambers_tuple.sort()                                     # сортировка
+nambers_tuple = (2**i for i in range(1, nambers + 1))
+list_nambers_tuple = list(nambers_tuple)
 print(list_nambers_tuple)
+
+# nambers = int(input("Список от 2^1 до 2^n, где n =  "))
+# nambers_tuple = {2**i for i in range(1, nambers + 1)}         # кортеж из от 2^1 до 2^n
+# list_nambers_tuple = list(nambers_tuple)                      # преобразовали в список
+# list_nambers_tuple.sort()                                     # сортировка
+# print(list_nambers_tuple)
 
 # Вариант 2
 # как работает этот генератор?
