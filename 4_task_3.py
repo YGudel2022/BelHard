900c7d60741d74046e647b01e6fe1d6b92599dbf
YGudel2022 <info@beliso.by>
2022-12-05 16:08:16 +0300

    merge Yuri end Gudel

--- 4_task_3.py
+++ 4_task_3.py
@@ -3,10 +3,15 @@
 # значениями вложенный словарь с ключами "name" и "email", а значения
 # для этих ключей будут браться с клавиатуры
 
-name = input("name: ")
-email = input("email: ")
-n = int(input("n: "))
-keys = (i for i in range(1, n + 1)) # ключи поместим в кортеж
-value = [(name, email)] # значение каждого ключа вводим с клавиатуры
-empty_k = dict.fromkeys(keys, value) # создаём словарь
+keys = {i: {'name': input("name: "), 'email': input("email: ")} for i in range(2)}
+empty_k = dict(keys)
 print(empty_k)
+# {0: {'name': 'ivan', 'email': 'inal@inal.by'}, 1: {'name': 'tom', 'email': 'tom@tom.by'}}
+
+# name = input("name: ")    # поставь объявление вложенного словаря в генератор внешнего словаря
+# email = input("email: ")
+# n = int(input("n: "))
+# keys = (i for i in range(1, n + 1)) # ключи поместим в кортеж
+# value = [(name, email)] # значение каждого ключа вводим с клавиатуры
+# empty_k = dict.fromkeys(keys, value) # создаём словарь
+# print(empty_k)
