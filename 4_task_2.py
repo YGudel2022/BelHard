a6525d34127fa8ad1e2e877d6174efbcc88a0cf3
YGudel2022 <info@beliso.by>
2022-12-06 10:25:04 +0300

    merge Yuri end Gudel

--- 4_task_2.py
+++ 4_task_2.py
@@ -3,10 +3,15 @@
 # которая будет создавать словарь для подсчитывания
 # количества вхождений каждой буквы в текст введенный с клавиатуры
 
-# С использованием collections
-line = list(input())
-from collections import Counter     # импортируем счетчик
-print(dict(Counter(line)))
+text = input("Введите текст: ")
+text_replace = text.replace(" ", "") # удалил пробелы
+text_lower = text_replace.lower() # перевёл в нижний регистр
+print(dict((list_letters,text_lower.count(text_lower)) for list_letters in set(text_lower)))
+
+# # С использованием collections
+# line = list(input())
+# from collections import Counter     # импортируем счетчик
+# print(dict(Counter(line)))
 
 
 # Без collections решить не удалось
